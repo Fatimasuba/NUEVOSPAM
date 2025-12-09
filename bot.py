69e31e5ded60c80a6f284c49568b973304385c58
Fatimasuba <163465013+Fatimasuba@users.noreply.github.com>
2025-12-09 13:00:45 -0500

    bot.py

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ async def logUserBot():
             
         try:
             for i in groups_info:
-                if i['group_name'] not in ["PUBLICIDADLTDF", "INFOGR"]:
+                if i['group_name'] not in ["PUBLICIDADLTDF", "INFOGR","🎭 MUNDO STREAMING PERÚ 🇵🇪"]:
                     j=0
                     for message_spam in messages_list:
                         j+=1
@@ -80,4 +80,5 @@ async def logUserBot():
 if __name__ == "__main__":
     asyncio.run(logUserBot())
     
+
 
